0095faffc28282ca6ebc4ab83d068bc13fedefd7
Seer7-SWE <seeralan006@gmail.com>
2025-07-10 18:18:58 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { AmazonStyleCart, WishList } from "./components";
+import Navbar from './components/Navbar';
+import Register from './components/Register';
 
 const App = () => {
     return (
         <Routes>
             <Route path="/" element={<WishList />} />
             <Route path="/cart" element={<AmazonStyleCart />} />
+            <Route path="/register" element={<Register />} />
         </Routes>
     );
 };
