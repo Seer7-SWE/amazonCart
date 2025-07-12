b812d149a798026e28e3f57b8c5d3c6e48976edf
Seer7-SWE <seeralan006@gmail.com>
2025-07-12 17:57:55 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { AmazonStyleCart, WishList } from "./components";
 import Register from './components/Register';
+import Checkout from "./Checkout.jsx";
 
 const App = () => {
     return (
@@ -8,6 +9,7 @@ const App = () => {
             <Route path="/" element={<WishList />} />
             <Route path="/cart" element={<AmazonStyleCart />} />
             <Route path="/register" element={<Register />} />
+            return <Checkout />;
         </Routes>
     );
 };
