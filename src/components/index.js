2041f6e7f26fc9ec7d62b4f536d1d4fca3d2deb7
Seer7-SWE <seeralan006@gmail.com>
2025-07-14 20:31:51 +0300

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
-export {  AmazonStyleCart } from './AmazonStyleCart';
-export {  WishList } from "./WishList";
-export {  Header } from "./Header";
-export { PurchaseModal } from "./PurchaseModal";
-export {  Sidebar } from "./Sidebar";
-export {  ProductModal } from "./ProductModal";
-export { Register } from "./Register"
+export   AmazonStyleCart from './AmazonStyleCart';
+export   WishList  from "./WishList";
+export   Header  from "./Header";
+export  PurchaseModal  from "./PurchaseModal";
+export   Sidebar  from "./Sidebar";
+export   ProductModal  from "./ProductModal";
+export  Register from "./Register"
