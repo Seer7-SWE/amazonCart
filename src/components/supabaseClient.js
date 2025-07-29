d1f4e1b2f3473593abefa4c4a3de9e4e4ea096f0
Seer7-SWE <seeralan006@gmail.com>
2025-07-29 12:55:13 +0300

    Update supabaseClient.js

--- src/components/supabaseClient.js
+++ src/components/supabaseClient.js
@@ -2,9 +2,9 @@
 import { createClient } from '@supabase/supabase-js';
 
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
